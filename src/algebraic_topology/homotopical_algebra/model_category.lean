2f4e446aa75d978387c4a1b1aac2548afc607542
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-02-22 23:00:26 +0100

    CM4 and lifting properties

--- src/algebraic_topology/homotopical_algebra/model_category.lean
+++ src/algebraic_topology/homotopical_algebra/model_category.lean
@@ -9,6 +9,7 @@ import category_theory.limits.shapes.pullbacks
 import category_theory.limits.opposites
 
 open category_theory
+open category_theory.category
 open category_theory.limits
 open opposite
 
@@ -24,7 +25,105 @@ variable {C}
 namespace quiver.hom
 
 def left_lifting_property {A B X Y : C} (i : A ⟶ B) (p : X ⟶ Y) :=
-  ∀ (f : A ⟶ X) (g : B ⟶ Y), ∃ (h : B ⟶ X), f = i ≫ h ∧ g = h ≫ p
+  ∀ (f : A ⟶ X) (g : B ⟶ Y) (hfg : f ≫ p = i ≫ g), ∃ (h : B ⟶ X), f = i ≫ h ∧ g = h ≫ p
+
+lemma left_lifting_property_iff_op {A B X Y : C} (i : A ⟶ B) (p : X ⟶ Y) :
+  i.left_lifting_property p ↔ p.op.left_lifting_property i.op :=
+begin
+  split,
+  { intros hip f g hfg,
+    rcases hip g.unop f.unop (by { exact congr_arg unop hfg.symm, }) with ⟨h, comm₁, comm₂⟩,
+    use h.op,
+    split,
+    { exact congr_arg op comm₂, },
+    { exact congr_arg op comm₁, }, },
+  { intros hip f g hfg,
+    rcases hip g.op f.op (by { exact congr_arg op hfg.symm, }) with ⟨h, comm₁, comm₂⟩,
+    use h.unop,
+    split,
+    { exact congr_arg unop comm₂, },
+    { exact congr_arg unop comm₁, }, },
+end
+
+lemma id_has_left_lifting_propery (A : C) {X Y : C} (p : X ⟶ Y) :
+  (𝟙 A).left_lifting_property p :=
+begin
+  intros f g hfg,
+  rw id_comp at hfg,
+  use f,
+  split,
+  { rw id_comp, },
+  { rw hfg, }
+end
+
+lemma id_has_right_lifting_property (X : C) {A B : C} (i : A ⟶ B) :
+  i.left_lifting_property (𝟙 X) :=
+by { rw left_lifting_property_iff_op, apply id_has_left_lifting_propery, }
+
+def is_retract {X Y X' Y' : C} (f : X ⟶ Y) (f' : X' ⟶ Y') := 
+  ∃ (a : X ⟶ X') (b : X' ⟶ X) (a' : Y ⟶ Y') (b' : Y' ⟶ Y),
+    (f ≫ a' = a ≫ f' ∧ b ≫ f = f' ≫ b') ∧ (a ≫ b = 𝟙 X ∧ a' ≫ b' = 𝟙 Y)
+
+def is_retract_iff_op {X Y X' Y' : C} (f : X ⟶ Y) (f' : X' ⟶ Y') :
+  f.is_retract f' ↔ f.op.is_retract f'.op :=
+begin
+  split,
+  { intro h,
+    rcases h with ⟨a, b, a', b', ⟨comm₁, comm₂⟩, ⟨r₁,r₂⟩⟩,
+    use [b'.op, a'.op, b.op, a.op],
+    split; split,
+    { exact congr_arg op comm₂, },
+    { exact congr_arg op comm₁, },
+    { exact congr_arg op r₂, },
+    { exact congr_arg op r₁, }, },
+  { intro h,
+    rcases h with ⟨a, b, a', b', ⟨comm₁, comm₂⟩, ⟨r₁,r₂⟩⟩,
+    use [b'.unop, a'.unop, b.unop, a.unop],
+    split; split,
+    { exact congr_arg unop comm₂, },
+    { exact congr_arg unop comm₁, },
+    { exact congr_arg unop r₂, },
+    { exact congr_arg unop r₁, }, },
+end
+
+lemma has_left_lifting_property_of_retract {A B A' B' X Y : C} (i : A ⟶ B) (p : X ⟶ Y) (i' : A' ⟶ B')
+  (hii' : i.is_retract i') (hi' : i'.left_lifting_property p) : i.left_lifting_property p :=
+begin
+  rcases hii' with ⟨a, b, a', b', ⟨comm₁, comm₂⟩, ⟨r₁,r₂⟩⟩,
+  intros f g hfg,
+  rcases hi' (b ≫ f) (b' ≫ g) (by { rw [assoc, hfg, ← assoc, comm₂, assoc], })
+    with ⟨h, ⟨lift₁, lift₂⟩⟩,
+  use a' ≫ h,
+  split,
+  { rw [← assoc, comm₁, assoc, ← lift₁, ← assoc, r₁, id_comp], },
+  { rw [assoc, ← lift₂, ← assoc, r₂, id_comp], }
+end
+
+lemma has_right_lifting_property_of_retract {A B X Y X' Y' : C} (p : X ⟶ Y) (i : A ⟶ B) (p' : X' ⟶ Y')
+  (hpp' : p.is_retract p') (hp' : i.left_lifting_property p') : i.left_lifting_property p :=
+begin
+  rw left_lifting_property_iff_op,
+  rw is_retract_iff_op at hpp',
+  apply has_left_lifting_property_of_retract p.op i.op p'.op hpp',
+  rw ← left_lifting_property_iff_op,
+  exact hp',
+end
+
+lemma has_left_lifting_property_of_is_iso {A B X Y : C} (i : A ⟶ B) [is_iso i] (p : X ⟶ Y) :
+  i.left_lifting_property p :=
+begin
+  refine has_left_lifting_property_of_retract i p (𝟙 A) _ (id_has_left_lifting_propery A p),
+  use [𝟙 A, 𝟙 A, inv i, i],
+  split; split,
+  { simp only [is_iso.hom_inv_id, comp_id], },
+  { refl, },
+  { rw id_comp, },
+  { simp only [is_iso.inv_hom_id], },
+end
+
+lemma has_right_lifting_property_of_is_iso {A B X Y : C} (p : X ⟶ Y) [is_iso p] (i : A ⟶ B) :
+  i.left_lifting_property p :=
+by { rw i.left_lifting_property_iff_op, apply has_left_lifting_property_of_is_iso, }
 
 end quiver.hom
 
@@ -107,8 +206,7 @@ def is_stable_by_cobase_change [has_pushouts C] :=
   ∀ (X X' Y : C) (f : X ⟶ Y) (g : X ⟶ X'), f ∈ F X Y → pushout.inr ∈ F X' (pushout f g)
 
 def is_stable_by_retract := ∀ (X Y : C) (f : X ⟶ Y),
-  (∃ (X' Y' : C) (f' : X' ⟶ Y') (hf' : f' ∈ F X' Y') (a : X ⟶ X') (b : X' ⟶ X) (a' : Y ⟶ Y') (b' : Y' ⟶ Y),
-    (f ≫ a' = a ≫ f' ∧ b ≫ f = f' ≫ b') ∧ (a ≫ b = 𝟙 X ∧ a' ≫ b' = 𝟙 Y)) → f ∈ F X Y    
+  (∃ (X' Y' : C) (f' : X' ⟶ Y') (hf' : f' ∈ F X' Y'), f.is_retract f') → f ∈ F X Y    
 
 lemma is_stable_by_retract_iff_op :
   is_stable_by_retract F ↔ is_stable_by_retract F.op :=
@@ -137,6 +235,17 @@ end
 def left_lifting_property (G : hom_class C) := ∀ (A B X Y : C) (i : A ⟶ B) (p : X ⟶ Y),
   i ∈ F A B → p ∈ G X Y → i.left_lifting_property p
 
+def left_lifting_property_iff_op (G : hom_class C) :
+  F.left_lifting_property G ↔ G.op.left_lifting_property F.op :=
+begin
+  split,
+  { intro h,
+    intros A B X Y i p hi hp,
+    simpa only [p.unop.left_lifting_property_iff_op] using h _ _ _ _ p.unop i.unop hp hi, },
+  { intro h,
+    intros A B X Y i p hi hp,
+    simpa only [i.left_lifting_property_iff_op] using h _ _ _ _ p.op i.op hp hi, }
+end
 
 end hom_class
 
@@ -225,8 +334,9 @@ def CM4b := data.trivial_cofibrations.left_lifting_property data.fibrations
 
 def CM4 := data.CM4a ∧ data.CM4b
 
-lemma CM4a_iff_op : data.CM4a ↔ data.op.CM4b := sorry
-lemma CM4b_iff_op : data.CM4b ↔ data.op.CM4a := sorry
+lemma CM4a_iff_op : data.CM4a ↔ data.op.CM4b := hom_class.left_lifting_property_iff_op _ _
+
+lemma CM4b_iff_op : data.CM4b ↔ data.op.CM4a := hom_class.left_lifting_property_iff_op _ _
 
 lemma CM4_iff_op : data.CM4 ↔ data.op.CM4 :=
 by { dsimp only [CM4], rw [data.CM4a_iff_op, data.CM4b_iff_op], finish, }
@@ -273,7 +383,6 @@ by { dsimp only [CM5], rw [data.CM5a_iff_op, data.CM5b_iff_op], finish, }
 
 lemma CM5_iff_unop : data'.CM5 ↔ data'.unop.CM5 :=
 by { rw [CM5_iff_op data'.unop, data'.op_unop], }
-
 
 end category_with_fib_cof_we
 
