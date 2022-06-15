1cdcebb88611d1c9c09d746367a4556a41cf6f33
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-06-15 18:45:54 +0200

    refactor

---
+++ src/for_mathlib/algebra/homology/hom_complex.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2022 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
+
+import 
+
+namespace algebra
+
+namespace homology
+
+end homology
+
+end algebra
