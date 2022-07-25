53244c3013452ab29bcbbd430d853405ec3ffc62
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-07-25 23:27:03 +0530

    hom_complex_exact

--- src/for_mathlib/algebra/homology/hom_complex_exact.lean
+++ src/for_mathlib/algebra/homology/hom_complex_exact.lean
@@ -22,7 +22,12 @@ namespace lift_cycle
 
 noncomputable theory
 
-def induction {n N : ℤ} (z : cocycle F G n) (l : lift_cycle z N)
+@[simps]
+def of_eq_N {n : ℤ} {z : cocycle F G n} {N N' : ℤ} (l : lift_cycle z N) (h : N=N') : lift_cycle z N' :=
+{ y := l.y,
+  hy := by { subst h, exact l.hy, }, }
+
+def induction {n N : ℤ} {z : cocycle F G n} (l : lift_cycle z N)
   (hX : projective (F.X N))
   (hG : G.cycles (N+n) = G.boundaries (N+n)) :
   {l' : lift_cycle z (N-1) // ∀ p q hpq, N < p → l'.y.v p q hpq = l.y.v p q hpq} :=
@@ -88,13 +93,148 @@ begin
     using hw₁ p q hpq hp⟩,
 end
 
+section
+
+variables {n N₀ : ℤ} {z : cocycle F G n} (l₀ : lift_cycle z N₀) (projF : ∀ p, p ≤ N₀ → projective (F.X p))
+variables (exactG : ∀ q, q ≤ N₀+n → G.cycles q = G.boundaries q)
+
+
+noncomputable
+def sequence : Π (k : ℕ), lift_cycle z (N₀-k)
+|0 := l₀.of_eq_N (by simp only [int.coe_nat_zero, sub_zero])
+|(k+1) := ((sequence k).induction (projF _ (by simp only [sub_le_self_iff, int.coe_nat_nonneg]))
+    (exactG _ (by simp only [add_le_add_iff_right, sub_le_self_iff,
+      int.coe_nat_nonneg]))).1.of_eq_N (by { simp only [int.coe_nat_succ], linarith, })
+
+lemma sequence_is_eventually_constant {k₁ k₂ : ℕ} (hk : k₁ ≤ k₂) (p q : ℤ) (hpq : q=p+(n-1)) (hp : N₀ < p+k₁) :
+  (sequence l₀ projF exactG (k₁)).y.v p q hpq = (sequence l₀ projF exactG k₂).y.v p q hpq :=
+begin
+  rw le_iff_exists_add at hk,
+  cases hk with d hd,
+  subst hd,
+  induction d with d hd,
+  { refl, },
+  { rw hd,
+    rw [show k₁+d.succ = k₁+d+1, by exact (add_assoc k₁ d 1).symm],
+    unfold sequence,
+    symmetry,
+    refine (induction (sequence l₀ projF exactG (k₁+d)) (projF _ _)
+      (exactG _ _)).2 p q hpq _,
+    { simp only [int.coe_nat_add, sub_le_self_iff], linarith, },
+    { simp only [int.coe_nat_add, add_le_add_iff_right, sub_le_self_iff], linarith, },
+    { have h := int.coe_zero_le d, simp only [int.coe_nat_add], linarith, }, },
+end
+
+def truncate (x : ℤ) : ℕ :=
+begin
+  by_cases 0 ≤ x,
+  { exact (int.eq_coe_of_zero_le h).some, },
+  { exact 0, },
+end
+
+def truncate_eq_zero (x : ℤ) (hx : x ≤ 0) : truncate x = 0 :=
+begin
+  dsimp [truncate],
+  split_ifs,
+  { let z := (int.eq_coe_of_zero_le h).some,
+    change z = 0,
+    have hz : x = z := (int.eq_coe_of_zero_le h).some_spec,
+    have hx : x = 0 := by linarith,
+    subst hx,
+    rw ← int.coe_nat_eq_coe_nat_iff,
+    exact hz.symm, },
+  { refl, },
+end
+
+def truncate_eq (x : ℕ) : truncate x = x :=
+begin
+  dsimp [truncate],
+  split_ifs,
+  { simpa only [int.coe_nat_eq_coe_nat_iff] using (int.eq_coe_of_zero_le h).some_spec.symm, },
+  { exfalso,
+    exact h (int.coe_zero_le _), },
+end
+
+lemma self_lt_truncate (x : ℤ) : x ≤ truncate x :=
+begin
+  by_cases x ≤ 0,
+  { rw truncate_eq_zero _ h,
+    exact h, },
+  { cases int.eq_coe_of_zero_le (show 0 ≤ x, by linarith) with m hm,
+    simp only [hm, truncate_eq], }
+end
+
+lemma truncate_lt_iff (x : ℤ) (y : ℕ) : truncate x ≤ y ↔ x ≤ (y : ℤ) :=
+begin
+  by_cases x ≤ 0,
+  { rw truncate_eq_zero _ h,
+    simp only [zero_le, true_iff],
+    linarith, },
+  { cases int.eq_coe_of_zero_le (show 0 ≤ x, by linarith) with m hm,
+    simp only [hm, truncate_eq, int.coe_nat_le], },
+end
+
+@[simp]
+def sequence_limit : cochain F G (n-1) := cochain.mk (λ p q hpq,
+  (sequence l₀ projF exactG (truncate (N₀+1-p))).y.v p q hpq)
+
+def sequence_limit_v_eq (p q : ℤ) (hpq : q=p+(n-1)) (k : ℕ) (hk : N₀+1 ≤ p + ↑k) :
+  (sequence_limit l₀ projF exactG).v p q hpq =
+  (sequence l₀ projF exactG k).y.v p q hpq :=
+begin
+  revert hk,
+  induction k with k hk,
+  { intro hp,
+    have h : truncate (N₀+1-p) = 0,
+    { apply truncate_eq_zero,
+      simp only [int.coe_nat_zero, add_zero] at hp,
+      linarith, },
+    simp only [sequence_limit, cochain.mk, cochain.v],
+    congr', },
+  { intro hp,
+    simp only [sequence_limit, cochain.mk, cochain.v],
+    dsimp only,
+    apply sequence_is_eventually_constant,
+    { rw truncate_lt_iff,
+      linarith, },
+    { have h := self_lt_truncate (N₀+1-p),
+      linarith, }, },
+end
+
+lemma δ_sequence_limit_v (p q : ℤ) (hpq : q=p+n) (k : ℕ) (hk : N₀+1 ≤ p+k):
+  (δ (n-1) n (sequence_limit l₀ projF exactG)).v p q hpq =
+  (δ (n-1) n (sequence l₀ projF exactG k).y).v p q hpq :=
+begin
+  simp only [δ_v (n-1) n (by linarith) _ p q hpq _ _ rfl rfl],
+  rw [sequence_limit_v_eq, sequence_limit_v_eq],
+  all_goals { linarith, },
+end
+
+lemma δ_sequence_limit : δ (n-1) n (sequence_limit l₀ projF exactG) = ↑z :=
+begin
+  ext p q hpq,
+  have h := self_lt_truncate (N₀+1-p),
+  rw δ_sequence_limit_v l₀ projF exactG p q hpq (truncate (N₀+1-p)) (by linarith),
+  exact (sequence l₀ projF exactG _).hy _ _ _ (by linarith),
+end
+
+end
+
 end lift_cycle
 
-lemma exact_of_bounded_above_termwise_projective (F G : cochain_complex C ℤ) [hF₁ : ∀ n, projective (F.X n)] (hF₂ : ∃ p₀, ∀ p, p≤p₀ → is_zero (F.X p))
+lemma exact_of_bounded_above_termwise_projective_src_and_acyclic_tgt (F G : cochain_complex C ℤ)
+  [hF₁ : ∀ n, projective (F.X n)] (hF₂ : ∃ N₀, ∀ p, N₀ < p → is_zero (F.X p))
   (hG : ∀ n, G.cycles n = G.boundaries n) {n m : ℤ} (z : hom_complex.cocycle F G n)
-  (hnm : n+1=m) : ∃ y, (hom_complex F G).d m n y = z.1 :=
+  (hnm : m+1=n) : ∃ y, (hom_complex F G).d m n y = z.1 :=
 begin
-  sorry
+  have hm : m = n-1 := by linarith,
+  subst hm,
+  cases hF₂ with N₀ hN₀,
+  let l₀ : lift_cycle z N₀ :=
+  { y := 0,
+    hy := λ p q hpq hp, (hN₀ p hp).eq_of_src _ _, },
+  use l₀.sequence_limit (λ p hp, hF₁ p) (λ q hq, hG q),
+  apply lift_cycle.δ_sequence_limit,
 end
 
 end hom_complex
