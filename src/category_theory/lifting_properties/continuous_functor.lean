8b4f5fe703ca666da556ef0ab82ac374ab978d7a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-08-29 21:49:07 +0200

    little progress

--- src/category_theory/lifting_properties/continuous_functor.lean
+++ src/category_theory/lifting_properties/continuous_functor.lean
@@ -16,6 +16,18 @@ variables {C : Type*} [category C] (P : morphism_property C)
   {α : Type u} [linear_order α] [is_well_order α (<)] (F : α ⥤ C)
   {β : Type*} [linear_order β] (h : principal_seg ((<) : β → β → Prop) ((<) : α → α → Prop))
 
+@[simps]
+def functor.well_order_inclusion_functor'
+  (h : initial_seg ((<) : β → β → Prop) ((<) : α → α → Prop)) : β ⥤ α :=
+begin
+  refine monotone.functor (_ : monotone h.to_rel_embedding.1.1),
+  intros b₁ b₂ r,
+  obtain (h₁ | h₂) := lt_or_eq_of_le r,
+  { rw ← h.to_rel_embedding.2 at h₁,
+    exact le_of_lt h₁, },
+  { subst h₂, },
+end
+
 include h
 
 @[simps]
@@ -56,20 +68,43 @@ begin
     exact h, },
 end
 
-variable (α)
+variables (α) [order_bot α]
 def is_stable_under_transfinite_composition (P : morphism_property C) : Prop :=
-  ∀ (a₀ : α) (ha₀ : ∀ (a₁ : α), a₀ ≤ a₁)
-    (F : α ⥤ C) (hF₁ : F.well_order_continuous)
+  ∀ (F : α ⥤ C) (hF₁ : F.well_order_continuous)
     (hF₂ : ∀ (a : α), P (F.map (hom_of_le (le_succ a))))
-    (c : cocone F) (hc : is_colimit c), P (c.ι.app a₀)
+    (c : cocone F) (hc : is_colimit c), P (c.ι.app ⊥)
+
+structure initial_seg' := (β : Type u) [lo : linear_order β]
+(h : initial_seg ((<) : β → β → Prop) ((<) : α → α → Prop))
+
+instance (B : initial_seg' α) : linear_order B.β := B.lo
 
 lemma llp_is_stable_under_transfinite_composition (P : morphism_property C) :
   P.llp_with.is_stable_under_transfinite_composition α :=
-λ a₀ ha₀ F hF₁ hF₂ c hc X Y p hp, ⟨λ f g, begin
+λ F hF₁ hF₂ c hc X Y p hp, ⟨λ f g, begin
   dsimp at g,
   intro sq,
-  sorry
+  suffices : ∀ (B : initial_seg' α),
+    ∃ (L : functor.well_order_inclusion_functor' B.h ⋙ F ⟶ (functor.const B.β).obj X)
+    (hL₁ : ∀ (b : B.β), f = F.map (hom_of_le bot_le) ≫ nat_trans.app L b),
+    ∀ (b : B.β), nat_trans.app L b ≫ p = c.ι.app (B.h b) ≫ g,
+  { rcases this (initial_seg'.mk α (by refl)) with ⟨L, hL₁, hL₂⟩,
+    dsimp [initial_seg.refl, functor.well_order_inclusion_functor'] at L hL₁ hL₂,
+    exact ⟨nonempty.intro
+    { l := begin
+        refine hc.desc (cocone.mk _ _),
+        exact
+        { app := L.app,
+          naturality' := λ a₁ a₂ r, by convert L.naturality r, },
+      end,
+      fac_left' := begin
+        have eq : (hom_of_le bot_le : (⊥ : α) ⟶ _) = 𝟙 ⊥ := subsingleton.elim _ _,
+        simp only [is_colimit.fac, hL₁ ⊥, eq, F.map_id, category.id_comp],
+      end,
+      fac_right' := hc.hom_ext (λ a, by simp only [is_colimit.fac_assoc, hL₂]), }⟩, },
+  sorry,
 end⟩
+
 
 end morphism_property
 
