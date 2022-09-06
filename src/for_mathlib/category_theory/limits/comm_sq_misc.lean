7b02cd12b82e44a9839d976b48212ec3f4a2905a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-09-06 14:38:10 +0200

    of_coprod_inl_with_id

--- src/for_mathlib/category_theory/limits/comm_sq_misc.lean
+++ src/for_mathlib/category_theory/limits/comm_sq_misc.lean
@@ -26,14 +26,20 @@ lemma of_coprod_inl_with_id {A B : C} (f : A ⟶ B) (X : C) [has_binary_coproduc
 is_pushout.of_is_colimit' (comm_sq.mk (coprod.inl_map _ _))
 (pushout_cocone.is_colimit_aux _ (λ s, coprod.desc s.inr (coprod.inr ≫ s.inl))
 (λ s, begin
-  sorry,
+  dsimp [comm_sq.cocone],
+  ext,
+  { simp only [coprod.map_desc, coprod.inl_desc, s.condition], },
+  { simp only [coprod.map_desc, category.id_comp, coprod.inr_desc], },
 end)
-(λ s, begin
-  sorry
-end)
-begin
-  sorry
-end)
+(λ s, by { dsimp [comm_sq.cocone], simp only [coprod.inl_desc], })
+(λ s m hm, begin
+  dsimp [comm_sq.cocone],
+  ext,
+  { simpa only [coprod.inl_desc] using hm walking_span.right, },
+  { have h := coprod.inr ≫= hm walking_span.left,
+    dsimp [comm_sq.cocone] at h,
+    simpa only [coprod.inr_desc, coprod.inr_map_assoc, category.id_comp] using h, },
+end))
 
 end is_pushout
 
