298057768d83ea8499a5358a52f3dc93689a39e1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-09-20 20:52:01 +0200

    uniq

--- src/for_mathlib/category_theory/localization/products.lean
+++ src/for_mathlib/category_theory/localization/products.lean
@@ -13,6 +13,9 @@ variables {C₁ D₁ C₂ D₂ : Type*} [category C₁] [category C₂] [categor
 
 namespace morphism_property
 
+class contains_identities {C : Type*} [category C] (W : morphism_property C) : Prop :=
+(id : ∀ (X : C), W (𝟙 X))
+
 def prod : morphism_property (C₁ × C₂) :=
 λ X Y f, W₁ f.fst ∧ W₂ f.snd
 
@@ -31,7 +34,7 @@ namespace strict_universal_property_fixed_target
 section
 
 variables {E : Type*} [category E] (F₁ : C₁ ⥤ D₁) (F₂ : C₂ ⥤ D₂)
-  (hW₁ : ∀ (X₁ : C₁), W₁ (𝟙 X₁)) (hW₂ : ∀ (X₂ : C₂), W₂ (𝟙 X₂))
+  [hW₁ : W₁.contains_identities] [hW₂ : W₂.contains_identities]
   (h₁ : strict_universal_property_fixed_target F₁ W₁ (C₂ ⥤ E))
   (h₂ : strict_universal_property_fixed_target F₂ W₂ (D₁ ⥤ E))
   (F : C₁ × C₂ ⥤ E) (hF : (W₁.prod W₂).is_inverted_by F)
@@ -39,54 +42,88 @@ variables {E : Type*} [category E] (F₁ : C₁ ⥤ D₁) (F₂ : C₂ ⥤ D₂)
 def prod.hom_mk {X₁ Y₁ : C₁} {X₂ Y₂ : C₂} (f₁ : X₁ ⟶ Y₁) (f₂ : X₂ ⟶ Y₂) :
   (⟨X₁, X₂⟩ : C₁ × C₂) ⟶ ⟨Y₁, Y₂⟩ := ⟨f₁, f₂⟩
 
-include hW₂ hF
+namespace prod
+
+include hF hW₂
 def lift₁ : D₁ ⥤ C₂ ⥤ E := h₁.lift (curry.obj F) (λ X₁ Y₁ f₁ hf₁, begin
   haveI : Π (Z₂ : C₂), is_iso (((curry.obj F).map f₁).app Z₂),
   { intro Z₂,
     apply hF,
-    exact ⟨hf₁, hW₂ _⟩, },
+    exact ⟨hf₁, morphism_property.contains_identities.id _⟩, },
     apply nat_iso.is_iso_of_is_iso_app,
 end)
 
-def fac₁ : F₁ ⋙ (lift₁ W₁ W₂ F₁ hW₂ h₁ F hF) = curry.obj F := h₁.fac _ _
+def fac₁ : F₁ ⋙ (lift₁ W₁ W₂ F₁ h₁ F hF) = curry.obj F := h₁.fac _ _
 
 def lift₁_obj_obj (X₁ : C₁) (X₂ : C₂) :
-  ((lift₁ W₁ W₂ F₁ hW₂ h₁ F hF).obj (F₁.obj X₁)).obj X₂ = F.obj ⟨X₁, X₂⟩ :=
-functor.congr_obj (functor.congr_obj (fac₁ W₁ W₂ F₁ hW₂ h₁ F hF) X₁) X₂
+  ((lift₁ W₁ W₂ F₁ h₁ F hF).obj (F₁.obj X₁)).obj X₂ = F.obj ⟨X₁, X₂⟩ :=
+functor.congr_obj (functor.congr_obj (fac₁ W₁ W₂ F₁ h₁ F hF) X₁) X₂
 
 @[simp]
 def lift₁_obj_map (X₁ : C₁) {X₂ Y₂ : C₂} (f₂ : X₂ ⟶ Y₂) :
-  ((lift₁ W₁ W₂ F₁ hW₂ h₁ F hF).obj (F₁.obj X₁)).map f₂ =
+  ((lift₁ W₁ W₂ F₁ h₁ F hF).obj (F₁.obj X₁)).map f₂ =
     eq_to_hom (by rw lift₁_obj_obj) ≫ F.map (prod.hom_mk (𝟙 X₁) f₂) ≫ eq_to_hom (by rw lift₁_obj_obj) :=
-functor.congr_map_conjugate (functor.congr_obj (fac₁ W₁ W₂ F₁ hW₂ h₁ F hF) X₁) f₂
+functor.congr_map_conjugate (functor.congr_obj (fac₁ W₁ W₂ F₁ h₁ F hF) X₁) f₂
 
 def lift₁_map₁ {X₁ Y₁ : C₁} (f₁ : X₁ ⟶ Y₁) (X₂ : C₂) :
-  ((lift₁ W₁ W₂ F₁ hW₂ h₁ F hF).map (F₁.map f₁)).app X₂ =
+  ((lift₁ W₁ W₂ F₁ h₁ F hF).map (F₁.map f₁)).app X₂ =
     eq_to_hom (by rw lift₁_obj_obj) ≫ F.map (prod.hom_mk f₁ (𝟙 X₂)) ≫ eq_to_hom (by rw lift₁_obj_obj) :=
 by simpa only [nat_trans.comp_app, eq_to_hom_app]
-  using congr_app (functor.congr_map_conjugate (fac₁ W₁ W₂ F₁ hW₂ h₁ F hF) f₁) X₂
+  using congr_app (functor.congr_map_conjugate (fac₁ W₁ W₂ F₁ h₁ F hF) f₁) X₂
 
 include hW₁
 
-def lift₂ : D₂ ⥤ D₁ ⥤ E := h₂.lift (lift₁ W₁ W₂ F₁ hW₂ h₁ F hF).flip (λ X₂ Y₂ f₂ hf₂, begin
-  haveI : ∀ (X₁ : D₁), is_iso (((lift₁ W₁ W₂ F₁ hW₂ h₁ F hF).flip.map f₂).app X₁),
+def lift₂ : D₂ ⥤ D₁ ⥤ E := h₂.lift (lift₁ W₁ W₂ F₁ h₁ F hF).flip (λ X₂ Y₂ f₂ hf₂, begin
+  haveI : ∀ (X₁ : D₁), is_iso (((lift₁ W₁ W₂ F₁ h₁ F hF).flip.map f₂).app X₁),
   { intro X₁,
     have hF₁ : ∃ (A₁ : C₁), X₁ = F₁.obj A₁ := sorry,
     cases hF₁ with A₁ hA₁,
     subst hA₁,
     simp only [functor.flip_map_app, lift₁_obj_map],
-    haveI := hF (prod.hom_mk (𝟙 A₁) f₂) ⟨hW₁ _, hf₂⟩,
+    haveI := hF (prod.hom_mk (𝟙 A₁) f₂) ⟨morphism_property.contains_identities.id _, hf₂⟩,
     apply_instance, },
   apply nat_iso.is_iso_of_is_iso_app,
 end)
 
-def lift₃ : D₁ × D₂ ⥤ E := uncurry.obj (lift₂ W₁ W₂ F₁ F₂ hW₁ hW₂ h₁ h₂ F hF).flip
+def lift₃ : D₁ × D₂ ⥤ E := uncurry.obj (lift₂ W₁ W₂ F₁ F₂ h₁ h₂ F hF).flip
+
+lemma uniq₁ (G G' : D₂ ⥤ D₁ ⥤ E) (eq : F₂ ⋙ G = F₂ ⋙ G') : G = G' := h₂.uniq _ _ eq
+
+omit hF hW₁ hW₂
+
+lemma flip_flip (H : D₁ ⥤ D₂ ⥤ E) : H.flip.flip = H :=
+functor.ext (λ X₁, (functor.ext (λ X₂, rfl) (by tidy))) (by tidy)
+
+lemma comp_comp_curry_flip_flip_eq_curry (H : D₁ × D₂ ⥤ E) :
+  F₁ ⋙ (F₂ ⋙ (curry.obj H).flip).flip = curry.obj (F₁.prod F₂ ⋙ H) :=
+functor.ext (λ X₁, (functor.ext (λ Y₁, rfl) (by tidy))) (by tidy)
+
+lemma uncurry_curry (H : D₁ × D₂ ⥤ E) : uncurry.obj (curry.obj H) = H :=
+functor.ext (λ X, by { cases X, refl, }) (by tidy)
+
+end prod
+
+include hW₁ hW₂
 
 def prod : strict_universal_property_fixed_target (F₁.prod F₂) (W₁.prod W₂) E :=
 { inverts_W := h₁.inverts_W.prod h₂.inverts_W,
-  lift := λ F hF, lift₃ W₁ W₂ F₁ F₂ hW₁ hW₂ h₁ h₂ F hF,
+  lift := λ F hF, prod.lift₃ W₁ W₂ F₁ F₂ h₁ h₂ F hF,
   fac := sorry,
-  uniq := sorry, }
+  uniq := λ H H' eq, begin
+    let G := (curry.obj H).flip,
+    let G' := (curry.obj H').flip,
+    suffices : G = G',
+    { rw [← prod.uncurry_curry H, ← prod.uncurry_curry H'],
+      congr' 1,
+      rw [← prod.flip_flip (curry.obj H), ← prod.flip_flip (curry.obj H')],
+      congr', },
+    apply h₂.uniq,
+    suffices : (F₂ ⋙ G).flip = (F₂ ⋙ G').flip,
+    { rw [← prod.flip_flip (F₂ ⋙ G), ← prod.flip_flip (F₂ ⋙ G'), this], },
+    apply h₁.uniq,
+    convert congr_arg curry.obj eq,
+    all_goals { apply prod.comp_comp_curry_flip_flip_eq_curry, },
+  end, }
 
 end
 
