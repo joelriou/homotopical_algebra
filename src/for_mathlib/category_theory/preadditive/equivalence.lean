ad18b3069e54b3a1ccdf3069518003b8dfd5be74
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-10-01 16:39:56 +0200

    an equivalence of categories between additive categories is additive

--- src/for_mathlib/category_theory/preadditive/equivalence.lean
+++ src/for_mathlib/category_theory/preadditive/equivalence.lean
@@ -1,4 +1,5 @@
 import category_theory.preadditive.additive_functor
+import category_theory.adjunction.limits
 import for_mathlib.category_theory.group_object
 
 namespace category_theory
@@ -9,10 +10,7 @@ open limits
 
 instance additive_of_binary_products {C D : Type*} [category C] [category D] [preadditive C] [preadditive D]
   (F : C ⥤ D) [is_equivalence F] [has_finite_products C] [has_finite_products D] : F.additive :=
-begin
-  haveI : ∀ (P : Type) [finite P], limits.preserves_limits_of_shape (discrete P) F := sorry,
-  exact F.additive_of_preserves_binary_products,
-end
+F.additive_of_preserves_binary_products
 
 end preadditive
 
