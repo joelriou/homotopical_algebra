273d044fd90d9e25dbdb9d7ec300f1802222251c
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-10-05 13:10:57 +0200

    tidied the code

--- src/for_mathlib/category_theory/triangulated/triangulated.lean
+++ src/for_mathlib/category_theory/triangulated/triangulated.lean
@@ -7,8 +7,73 @@ namespace category_theory
 open limits category preadditive triangulated
 open_locale zero_object
 
-variables (C : Type*) [category C] [preadditive C] [has_zero_object C] [has_shift C ℤ]
+variables {C : Type*} [category C] [preadditive C] [has_zero_object C] [has_shift C ℤ]
   [∀ (n : ℤ), functor.additive (shift_functor C n)] [pretriangulated C]
+
+namespace triangulated
+
+section
+
+variables {X₁ X₂ X₃ Z₁₂ Z₂₃ Z₁₃ : C} {u₁₂ : X₁ ⟶ X₂} {u₂₃ : X₂ ⟶ X₃} {u₁₃ : X₁ ⟶ X₃}
+  (comm : u₁₂ ≫ u₂₃ = u₁₃)
+  {v₁₂ : X₂ ⟶ Z₁₂} {w₁₂ : Z₁₂ ⟶ X₁⟦(1 : ℤ)⟧} (h₁₂ : triangle.mk C u₁₂ v₁₂ w₁₂ ∈ dist_triang C)
+  {v₂₃ : X₃ ⟶ Z₂₃} {w₂₃ : Z₂₃ ⟶ X₂⟦(1 : ℤ)⟧} (h₂₃ : triangle.mk C u₂₃ v₂₃ w₂₃ ∈ dist_triang C)
+  {v₁₃ : X₃ ⟶ Z₁₃} {w₁₃ : Z₁₃ ⟶ X₁⟦(1 : ℤ)⟧} (h₁₃ : triangle.mk C u₁₃ v₁₃ w₁₃ ∈ dist_triang C)
+
+include comm h₁₂ h₂₃ h₁₃
+
+def octahedron_exists : Prop :=
+∃ (m₁ : Z₁₂ ⟶ Z₁₃) (m₃ : Z₁₃ ⟶ Z₂₃) (comm₁ : v₁₂ ≫ m₁ = u₂₃ ≫ v₁₃)
+    (comm₂ : w₁₂ = m₁ ≫ w₁₃) (comm₃ : v₁₃ ≫ m₃ = v₂₃) (comm₄ : w₁₃ ≫ u₁₂⟦1⟧' = m₃ ≫ w₂₃),
+    triangle.mk C m₁ m₃ (w₂₃ ≫ v₁₂⟦1⟧') ∈ dist_triang C
+
+namespace octahedron_exists
+
+variables {comm h₁₂ h₂₃ h₁₃} (h : octahedron_exists comm h₁₂ h₂₃ h₁₃)
+
+def m₁ : Z₁₂ ⟶ Z₁₃ := h.some
+def m₃ : Z₁₃ ⟶ Z₂₃ := h.some_spec.some
+
+@[simps]
+def triangle : triangle C :=
+triangle.mk C h.m₁ h.m₃ (w₂₃ ≫ v₁₂⟦1⟧')
+
+lemma triangle_distinguished : h.triangle ∈ dist_triang C :=
+h.some_spec.some_spec.some_spec.some_spec.some_spec.some_spec
+
+@[simps]
+def triangle_morphism₁ : triangle.mk C u₁₂ v₁₂ w₁₂ ⟶ triangle.mk C u₁₃ v₁₃ w₁₃ :=
+{ hom₁ := 𝟙 X₁,
+  hom₂ := u₂₃,
+  hom₃ := h.m₁,
+  comm₁' := by { dsimp, rw [id_comp, comm], },
+  comm₂' := h.some_spec.some_spec.some,
+  comm₃' := begin
+    dsimp,
+    simpa only [functor.map_id, comp_id]
+      using h.some_spec.some_spec.some_spec.some,
+  end }
+
+@[simps]
+def triangle_morphism₂ : triangle.mk C u₁₃ v₁₃ w₁₃ ⟶ triangle.mk C u₂₃ v₂₃ w₂₃ :=
+{ hom₁ := u₁₂,
+  hom₂ := 𝟙 X₃,
+  hom₃ := h.m₃,
+  comm₁' := by { dsimp, rw [comp_id, comm], },
+  comm₂' := begin
+    dsimp,
+    simpa only [id_comp] using
+      h.some_spec.some_spec.some_spec.some_spec.some,
+  end,
+  comm₃' := h.some_spec.some_spec.some_spec.some_spec.some_spec.some, }
+
+end octahedron_exists
+
+end
+
+end triangulated
+
+variable (C)
 
 class triangulated :=
 (octahedron' : ∀ ⦃X₁ X₂ X₃ Z₁₂ Z₂₃ Z₁₃ : C⦄ ⦃u₁₂ : X₁ ⟶ X₂⦄ ⦃u₂₃ : X₂ ⟶ X₃⦄ ⦃u₁₃ : X₁ ⟶ X₃⦄
@@ -16,9 +81,7 @@ class triangulated :=
   ⦃v₁₂ : X₂ ⟶ Z₁₂⦄ ⦃w₁₂ : Z₁₂ ⟶ X₁⟦1⟧⦄ (h₁₂ : triangle.mk C u₁₂ v₁₂ w₁₂ ∈ dist_triang C)
   ⦃v₂₃ : X₃ ⟶ Z₂₃⦄ ⦃w₂₃ : Z₂₃ ⟶ X₂⟦1⟧⦄ (h₂₃ : triangle.mk C u₂₃ v₂₃ w₂₃ ∈ dist_triang C)
   ⦃v₁₃ : X₃ ⟶ Z₁₃⦄ ⦃w₁₃ : Z₁₃ ⟶ X₁⟦1⟧⦄ (h₁₃ : triangle.mk C u₁₃ v₁₃ w₁₃ ∈ dist_triang C),
-  ∃ (m₁ : Z₁₂ ⟶ Z₁₃) (m₃ : Z₁₃ ⟶ Z₂₃) (comm₁ : v₁₂ ≫ m₁ = u₂₃ ≫ v₁₃)
-    (comm₂ : w₁₂ = m₁ ≫ w₁₃) (comm₃ : v₁₃ ≫ m₃ = v₂₃) (comm₄ : w₁₃ ≫ u₁₂⟦1⟧' = m₃ ≫ w₂₃),
-    triangle.mk C m₁ m₃ (w₂₃ ≫ v₁₂⟦1⟧') ∈ dist_triang C)
+  triangulated.octahedron_exists comm h₁₂ h₂₃ h₁₃)
 
 variable {C}
 
@@ -38,42 +101,22 @@ variables {X₁ X₂ X₃ Z₁₂ Z₂₃ Z₁₃ : C} {u₁₂ : X₁ ⟶ X₂}
 
 include comm h₁₂ h₂₃ h₁₃
 
-def m₁ : Z₁₂ ⟶ Z₁₃ := (octahedron comm h₁₂ h₂₃ h₁₃).some
-def m₃ : Z₁₃ ⟶ Z₂₃ := (octahedron comm h₁₂ h₂₃ h₁₃).some_spec.some
+def m₁ : Z₁₂ ⟶ Z₁₃ := (octahedron comm h₁₂ h₂₃ h₁₃).m₁
+def m₃ : Z₁₃ ⟶ Z₂₃ := (octahedron comm h₁₂ h₂₃ h₁₃).m₃
 
 @[simps]
-def triangle : triangle C :=
-triangle.mk C (m₁ comm h₁₂ h₂₃ h₁₃) (m₃ comm h₁₂ h₂₃ h₁₃) (w₂₃ ≫ v₁₂⟦1⟧')
+def triangle : triangle C := (octahedron comm h₁₂ h₂₃ h₁₃).triangle
 
 lemma triangle_distinguished : triangle comm h₁₂ h₂₃ h₁₃ ∈ dist_triang C :=
-(octahedron comm h₁₂ h₂₃ h₁₃).some_spec.some_spec.some_spec.some_spec.some_spec.some_spec
+(octahedron comm h₁₂ h₂₃ h₁₃).triangle_distinguished
 
 @[simps]
 def triangle_morphism₁ : triangle.mk C u₁₂ v₁₂ w₁₂ ⟶ triangle.mk C u₁₃ v₁₃ w₁₃ :=
-{ hom₁ := 𝟙 X₁,
-  hom₂ := u₂₃,
-  hom₃ := m₁ comm h₁₂ h₂₃ h₁₃,
-  comm₁' := by { dsimp, rw [id_comp, comm], },
-  comm₂' := (octahedron comm h₁₂ h₂₃ h₁₃).some_spec.some_spec.some,
-  comm₃' := begin
-    dsimp,
-    simpa only [functor.map_id, comp_id]
-      using (octahedron comm h₁₂ h₂₃ h₁₃).some_spec.some_spec.some_spec.some,
-  end }
+(octahedron comm h₁₂ h₂₃ h₁₃).triangle_morphism₁
 
 @[simps]
 def triangle_morphism₂ : triangle.mk C u₁₃ v₁₃ w₁₃ ⟶ triangle.mk C u₂₃ v₂₃ w₂₃ :=
-{ hom₁ := u₁₂,
-  hom₂ := 𝟙 X₃,
-  hom₃ := m₃ comm h₁₂ h₂₃ h₁₃,
-  comm₁' := by { dsimp, rw [comp_id, comm], },
-  comm₂' := begin
-    dsimp,
-    simpa only [id_comp] using
-      (octahedron comm h₁₂ h₂₃ h₁₃).some_spec.some_spec.some_spec.some_spec.some,
-  end,
-  comm₃' := (octahedron comm h₁₂ h₂₃ h₁₃).some_spec.some_spec.some_spec.some_spec.some_spec.some, }
-
+(octahedron comm h₁₂ h₂₃ h₁₃).triangle_morphism₂
 
 end octahedron
 
