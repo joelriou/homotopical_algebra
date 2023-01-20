65ace29b7153fa441aecde40d789f793aa03d110
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-01-20 19:06:36 +0100

    wip

--- src/for_mathlib/algebra/homology/derivability_structure_injective.lean
+++ src/for_mathlib/algebra/homology/derivability_structure_injective.lean
@@ -14,6 +14,9 @@ namespace homological_complex
 class is_termwise_injective (K : homological_complex C c) : Prop :=
 (X_injective [] : ∀ (n : ι), injective (K.X n))
 
+instance (K : homological_complex C c) (n : ι) [K.is_termwise_injective] : injective (K.X n) :=
+is_termwise_injective.X_injective K n
+
 instance zero_is_termwise_injective :
   (0 : homological_complex C c).is_termwise_injective :=
 ⟨λ n, injective.of_iso (homological_complex.eval C c n).map_zero_object.symm
@@ -39,6 +42,11 @@ begin
     refine ⟨λ i, _⟩,
     apply homological_complex.is_termwise_injective.X_injective K _, },
 end
+
+instance mapping_cone_is_termwise_injective {K L : cochain_complex C ℤ} (f : K ⟶ L)
+  [K.is_termwise_injective] [L.is_termwise_injective] :
+  (mapping_cone f).is_termwise_injective :=
+⟨λ n, by { dsimp [mapping_cone], apply_instance, }⟩
 
 end cochain_complex
 
@@ -71,7 +79,19 @@ instance is_termwise_injective_is_triangulated_subcategory' :
     change K⟦n⟧.is_termwise_injective,
     simpa only [cochain_complex.shift_is_termwise_injective_iff] using h,
   end,
-  distinguished_cocone_triangle' := sorry, }
+  distinguished_cocone_triangle' := begin
+    rintro ⟨⟨K : cochain_complex C ℤ⟩, hK⟩ ⟨⟨L : cochain_complex C ℤ⟩, hL⟩ hK' hL',
+    haveI : K.is_termwise_injective := hK',
+    haveI : L.is_termwise_injective := hL',
+    rintro (f : (homotopy_category.quotient _ _).obj K ⟶ (homotopy_category.quotient _ _).obj L),
+    obtain ⟨f, rfl⟩ := (homotopy_category.quotient _ _).map_surjective f,
+    exact ⟨⟨(homotopy_category.quotient _ _).obj (cochain_complex.mapping_cone f),
+      cochain_complex.mapping_cone_is_plus f hK hL⟩,
+      (infer_instance : (cochain_complex.mapping_cone f).is_termwise_injective),
+      (homotopy_category.quotient _ _).map (cochain_complex.mapping_cone.inr f),
+      (homotopy_category.quotient _ _).map (cochain_complex.mapping_cone.δ f),
+      by { erw triangle_distinguished_iff, exact ⟨_, _, f, ⟨iso.refl _⟩⟩, }⟩,
+  end, }
 
 instance test : (termwise_injective.ι : _ ⥤ homotopy_category.plus C).is_triangulated := infer_instance
 
